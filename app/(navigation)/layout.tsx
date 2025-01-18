604dd02f7965adc1107474fc4d97773f0284e518
Hartaithan <hartaithan@gmail.com>
2025-01-18 15:01:43 +0900

    migrate to external portfolio background

--- app/(navigation)/layout.tsx
+++ app/(navigation)/layout.tsx
@@ -1,6 +1,6 @@
-import Background from "@/components/background";
 import LayoutTransition from "@/components/layout-transition";
 import Navigation from "@/components/nav";
+import Script from "next/script";
 import type { FC, PropsWithChildren } from "react";
 
 const NavigationLayout: FC<PropsWithChildren> = (props) => {
@@ -9,7 +9,10 @@ const NavigationLayout: FC<PropsWithChildren> = (props) => {
     <>
       <Navigation />
       <LayoutTransition>{children}</LayoutTransition>
-      <Background />
+      <Script
+        src="https://hartaithan.github.io/portfolio-background/init.iife.js"
+        strategy="lazyOnload"
+      />
     </>
   );
 };
